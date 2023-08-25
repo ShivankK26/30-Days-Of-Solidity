148598ed3425ffee5afb750ca134af0c1c12a693
Shivank Kapur <115289871+ShivankK26@users.noreply.github.com>
2023-08-25 17:40:03 +0530

    Create Answer.sol

---
+++ Answer.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day10 {
+  function prime(uint256 n) public pure returns(uint256){
+    if(n <= 1){
+      return 0;
+    }
+    for(uint i = 2; i < n; i++){
+      if(n % i == 0){
+        return 0
+      }
+    }
+    return 1;
+  }
+}
